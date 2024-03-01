a131dbebc00da6463d52473877d153cde9a7b085
Dyst <50362174+Dyastinn@users.noreply.github.com>
2024-03-02 02:54:51 +0800

    Added Controllers

---
+++ RecycleWeb/Controllers/RecyclableItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecycleWeb.Controllers
+{
+    public class RecyclableItemController : Controller
+    {
+        // GET: RecyclableItem
+        public ActionResult List() {
+            return View();
+        }
+        public ActionResult Add() {
+            return View();
+        }
+        public ActionResult Edit() {
+            return View();
+        }
+        public ActionResult Delete() {
+            return View();
+        }
+    }
+}
---
+++ RecycleWeb/Controllers/RecyclableTypeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecycleWeb.Controllers
+{
+    public class RecyclableTypeController : Controller
+    {
+        // GET: Recycle
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Add() {
+            return View();
+        }
+        public ActionResult Edit() {
+            return View();
+        }
+        public ActionResult Delete() {
+            return View();
+        }
+    }
+}
