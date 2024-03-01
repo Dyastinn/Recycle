f8aa7895303b2073b46452a03c8d339f081f6fef
Dyst <50362174+Dyastinn@users.noreply.github.com>
2024-03-02 03:26:21 +0800

    Edited the views

--- RecycleWeb/Controllers/RecyclableItemController.cs
+++ RecycleWeb/Controllers/RecyclableItemController.cs
@@ -6,20 +6,59 @@ using System.Web.Mvc;
 
 namespace RecycleWeb.Controllers
 {
+    
     public class RecyclableItemController : Controller
     {
+        ApplicationDbContext _db = new ApplicationDbContext();
         // GET: RecyclableItem
         public ActionResult List() {
-            return View();
+            var ItemList = _db.RecyclableItems.ToList();
+            return View(ItemList);
         }
+
+        [HttpGet]
         public ActionResult Add() {
             return View();
         }
-        public ActionResult Edit() {
+
+        [HttpPost]
+        public ActionResult Add(RecyclableItem item) {
+
+            _db.RecyclableItems.Add(item);
+            _db.SaveChanges();
+ 
             return View();
         }
-        public ActionResult Delete() {
-            return View();
+
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var item = _db.RecyclableItems.Where(x => x.Id == id).FirstOrDefault();
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RecyclableItem item) {
+            var data = _db.RecyclableItems.Where(x => x.Id == item.Id).FirstOrDefault();
+            if(data != null) {
+                data.Weight = item.Weight;
+                data.RecyclableTypeId = item.RecyclableTypeId;
+                data.ItemDescription = item.ItemDescription;
+                data.ComputedRate = item.ComputedRate;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("List");
+
+
+        }
+
+
+
+        public ActionResult Delete(int id) {
+            var data = _db.RecyclableItems.Where(x => x.Id == id).FirstOrDefault();
+            _db.RecyclableItems.Remove(data);
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
         }
     }
 }
--- RecycleWeb/Controllers/RecyclableTypeController.cs
+++ RecycleWeb/Controllers/RecyclableTypeController.cs
@@ -9,18 +9,56 @@ namespace RecycleWeb.Controllers
     public class RecyclableTypeController : Controller
     {
         // GET: Recycle
-        public ActionResult List()
-        {
-            return View();
+        ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: RecyclableType
+        public ActionResult List() {
+            var TypeList = _db.RecyclableTypes.ToList();
+            return View(TypeList);
         }
+
+        [HttpGet]
         public ActionResult Add() {
             return View();
         }
-        public ActionResult Edit() {
+
+        [HttpPost]
+        public ActionResult Add(RecyclableType item) {
+
+            _db.RecyclableTypes.Add(item);
+            _db.SaveChanges();
+ 
             return View();
         }
-        public ActionResult Delete() {
-            return View();
+
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var item = _db.RecyclableTypes.Where(x => x.Id == id).FirstOrDefault();
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RecyclableType item) {
+            var data = _db.RecyclableTypes.Where(x => x.Id == item.Id).FirstOrDefault();
+            if(data != null) {
+                data.Type = item.Type;
+                data.Rate = item.Rate;
+                data.MinKg = item.MinKg;
+                data.MaxKg = item.MaxKg;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("List");
+
+
+        }
+
+
+
+        public ActionResult Delete(int id) {
+            var data = _db.RecyclableTypes.Where(x => x.Id == id).FirstOrDefault();
+            _db.RecyclableTypes.Remove(data);
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
         }
     }
 }
